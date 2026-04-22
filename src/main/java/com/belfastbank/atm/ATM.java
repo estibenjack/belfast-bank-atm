6d0ab63fbcc97bd8d1bcb50ab5d8bef16e9b990f
Steven <156148848+estibenjack@users.noreply.github.com>
2026-04-22 21:25:43 +0100

    implement showMenu method for ATM class

--- src/main/java/com/belfastbank/atm/ATM.java
+++ src/main/java/com/belfastbank/atm/ATM.java
@@ -5,6 +5,7 @@ import com.belfastbank.model.Customer;
 import com.belfastbank.service.AccountService;
 import com.belfastbank.service.CustomerService;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class ATM {
@@ -70,10 +71,74 @@ public class ATM {
     }
 
     public void selectAccount() {
+        List<Account> availableAccounts = loggedInCustomer.getAccounts();
+        if (availableAccounts.size()==1) {
+            this.selectedAccount=availableAccounts.getFirst();
+        } else {
+            printHeading("Select Account");
+            for (int i=0; i<availableAccounts.size(); i++){
+                System.out.printf("%d. %s (****%s)%n", i+1, availableAccounts.get(i).getAccountType(), availableAccounts.get(i).getAccountNo().substring(4));
+            }
+            System.out.print("\nPlease select: ");
 
+            int choice=-1;
+            while (true) {
+                try {
+                    choice = Integer.parseInt(scanner.nextLine().trim());
+                    if (choice >= 1 && choice <= availableAccounts.size()) {
+                        this.selectedAccount = availableAccounts.get(choice - 1);
+                        break;
+                    } else {
+                        System.out.print("Invalid selection. Please try again: ");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.print("Invalid selection. Please try again: ");
+                }
+            }
+        }
+        showMenu();
     }
 
     public void showMenu(){
+        int choice=-1;
+        while (true) {
+            printHeading("Belfast Bank ATM");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Deposit");
+            System.out.println("4. Mini Statement");
+            System.out.println("5. Exit");
+            printDivider();
+            System.out.print("\nPlease select: ");
+            try {
+                choice = Integer.parseInt(scanner.nextLine().trim());
+                if (choice >= 1 && choice <= 5) {
+                   switch (choice) {
+                       case 1:
+                           handleBalance();
+                           break;
+                       case 2:
+                           handleWithdraw();
+                           break;
+                       case 3:
+                           handleDeposit();
+                           break;
+                       case 4:
+                           handleMiniStatement();
+                           break;
+                       case 5:
+                           System.out.println("Thank you for using Belfast Bank. Goodbye!");
+                           return;
+                       default:
+                           break;
+                   }
+                } else {
+                    System.out.print("Invalid selection. Please try again: ");
+                }
+            } catch (NumberFormatException e) {
+                System.out.print("Invalid selection. Please try again: ");
+            }
+        }
 
     }
 
